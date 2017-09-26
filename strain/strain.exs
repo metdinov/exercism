f94c749df059264dbb60cdfca0db59fa18ec1338
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2017-09-26 14:03:58 -0300

    2nd version using tail recursion

--- strain/strain.exs
+++ strain/strain.exs
@@ -5,12 +5,21 @@ defmodule Strain do
 
   Do not use `Enum.filter`.
 
-  First version using for comprehension
+  Second version using tail recursion
   """
   @spec keep(list :: list(any), fun :: ((any) -> boolean)) :: list(any)
   def keep(list, fun) do
-    for elem <- list, fun.(elem), do: elem
+    recurse(list, fun, [])
   end
+
+  defp recurse([head | tail], fun, result) do
+    if fun.(head) do
+      recurse(tail, fun, [head | result])
+    else
+      recurse(tail, fun, result)
+    end
+  end
+  defp recurse([], fun, result), do: result |> Enum.reverse
 
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
