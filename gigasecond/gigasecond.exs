c43ae8046c7d58b2b95491bd0cffeacf8ac9e4fa
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2017-11-08 14:28:33 -0300

    don't try date arithmetic manually! use the api luke

---
+++ gigasecond/gigasecond.exs
@@ -0,0 +1,13 @@
+defmodule Gigasecond do
+  @doc """
+  Calculate a date one billion seconds after an input date.
+  """
+  @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime
+
+  def from(date) do
+    date
+    |> NaiveDateTime.from_erl!
+    |> NaiveDateTime.add(1_000_000_000)
+    |> NaiveDateTime.to_erl
+  end
+end
