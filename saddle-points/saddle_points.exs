4a2f5e1e185b300eb452ec9d0a29c6ed12b963e0
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2017-12-01 13:11:32 -0300

    Enum.find_index stops when it finds first x where func(x) returns true

--- saddle-points/saddle_points.exs
+++ saddle-points/saddle_points.exs
@@ -7,14 +7,14 @@ defmodule SaddlePoints do
   def rows(str) do
     str
     |> String.split("\n")
-    |> Enum.map(&to_int_list(&1))
+    |> Enum.map(&to_int_list/1)
   end
 
   @spec to_int_list(String.t()) :: [integer]
-  defp to_int_list(str) do
-    str
+  defp to_int_list(row) do
+    row
     |> String.split
-    |> Enum.map(&String.to_integer(&1))
+    |> Enum.map(&String.to_integer/1)
   end
 
   @doc """
@@ -23,7 +23,10 @@ defmodule SaddlePoints do
   """
   @spec columns(String.t()) :: [[integer]]
   def columns(str) do
-
+    str
+    |> rows
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
   end
 
   @doc """
@@ -32,6 +35,23 @@ defmodule SaddlePoints do
   """
   @spec saddle_points(String.t()) :: [{integer, integer}]
   def saddle_points(str) do
+    row_list = rows(str)
+    column_list = columns(str)
+    points =
+      for {row, index} <- Enum.with_index(row_list) do
+        for i <- index_of_func(row, &Enum.max/1) do
+          for j <- index_of_func(Enum.at(column_list, i), &Enum.min/1),
+              j == index,
+              do: {index, i}
+        end
+      end
+    List.flatten points
+  end
 
+  defp index_of_func(int_list, func) do
+    value = func.(int_list)
+    int_list
+    |> Enum.with_index
+    |> Enum.filter_map(fn {x, _} -> x == value end, fn {_, i} -> i end)
   end
 end
