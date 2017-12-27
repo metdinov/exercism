5ecbafd3148dd44c3a5e67361396f23dd36aad87
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2017-12-27 09:15:25 -0300

    not quite my functional tempo, but it works

--- rail-fence-cipher/rail_fence_cipher.exs
+++ rail-fence-cipher/rail_fence_cipher.exs
@@ -7,22 +7,44 @@ defmodule RailFenceCipher do
     str
   end
   def encode(str, rails) do
-    fence = 
-      Enum.concat((1..rails), (rails-1)..2)
-      |> Stream.cycle
-      |> Enum.take(String.length(str))
+    fence = build_fence(rails, String.length(str))
 
     for rail <- 1..rails,
-        indeces = Enum.filter(Enum.with_index(fence), fn {i, _} -> i == rail end),
-        into: "",
-        do: for {_rail, ind} <- indeces, into: "", do: String.at(str, ind)
+        indices = find_indices(fence, rail),
+        into: "" do
+          for ind <- indices, into: "", do: String.at(str, ind)      
+    end
+  end
+
+  defp build_fence(rails, len) do
+    Enum.concat((1..rails), (rails-1)..2)
+    |> Stream.cycle
+    |> Enum.take(len)
+  end
+
+  # Helper function (Enum.find_index stops at first match)
+  defp find_indices(list, value) do
+    list
+    |> Enum.with_index
+    |> Enum.filter(fn {x, _} -> x == value end)
+    |> Enum.map(fn {_, i} -> i end)
   end
 
   @doc """
   Decode a given rail fence ciphertext to the corresponding plaintext
   """
   @spec decode(String.t, pos_integer) :: String.t
-  def decode(str, rails) do
+  def decode(str, 1) do
     str
+  end
+  def decode(str, rails) do
+    rails
+    |> build_fence(String.length(str))
+    |> Enum.with_index
+    |> Enum.sort
+    |> Enum.zip(String.graphemes(str))
+    |> Enum.sort_by(fn {{_rail, i}, _char} -> i end)
+    |> Enum.map(fn {{_rail, _i}, char} -> char end)
+    |> Enum.join
   end
 end
