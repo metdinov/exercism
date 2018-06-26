cee958e1a7cc51985152664cea53e3bede02392b
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2018-06-26 07:08:51 -0300

    Not passing all tests. WIP

--- connect/connect.exs
+++ connect/connect.exs
@@ -7,62 +7,57 @@ defmodule Connect do
 
   @spec result_for([String.t()]) :: :none | :black | :white
   def result_for(board) do
-    white_stones = %{}
-    black_stones = %{}
     span = length(board) - 1
-    for {line, row} <- Enum.with_index(board) do
-      for {cell, col} <- line |> String.graphemes |> Enum.with_index do
-        case cell do
-          "O" -> Map.update(white_stones, row, MapSet.new, &(MapSet.put(&1, col)))
-          "X" -> Map.update(black_stones, col, MapSet.new, &(MapSet.put(&1, row)))
-          _ -> nil
-        end
-      end
-    end
+  
+    board
+    |> Enum.with_index
+    |> Enum.reduce(%{}, fn {line, row}, clusters -> read_line(line, clusters, row) end)
+    |> find_winner(span)
+  end
 
+  def read_line(line, clusters, row, col \\ 0)
+  def read_line("O" <> rest, clusters, row, col) do
+    new_clusters = add_stone(Map.get(clusters, :white, MapSet.new), {row, col})
+    read_line(rest, Map.put(clusters, :white, new_clusters), row, col + 1)
+  end
+  def read_line("X" <> rest, clusters, row, col) do
+    new_clusters = add_stone(Map.get(clusters, :black, MapSet.new), {row, col})
+    read_line(rest, Map.put(clusters, :black, new_clusters), row, col + 1)
+  end
+  def read_line(<<_cell::utf8, rest::binary>>, clusters, row, col) do
+    read_line(rest, clusters, row, col + 1)
+  end
+  def read_line("", clusters, _row, _col) do
+    clusters
+  end
+
+  def add_stone(clusters, stone) do
+    cluster = 
+      Enum.find(clusters, 
+                MapSet.new,
+                fn c -> not MapSet.disjoint?(c, neighbors(stone)) end)
+    
+    # WTB lens
+    clusters
+    |> MapSet.delete(cluster)
+    |> MapSet.put(MapSet.put(cluster, stone))
+  end
+
+  def neighbors({row, col}) do
+    MapSet.new([{row-1, col}, {row-1, col+1}, {row, col-1}, 
+            {row, col+1}, {row+1, col-1}, {row+1, col}])
+  end
+
+  def find_winner(clusters, span) do
     cond do
-      winner?(white_stones, span) -> :white
-      winner?(black_stones, span) -> :black
+      Enum.any?(Map.get(clusters, :white, []), &connected_cluster?(&1, span, 0)) -> :white
+      Enum.any?(Map.get(clusters, :black, []), &connected_cluster?(&1, span, 1)) -> :black
       true -> :none
     end
   end
 
-  defp winner?(stones, rank) do
-    winner?(stones, rank-1, Map.get(stones, rank, MapSet.new))
-  end
-  defp winner?(stones, rank, connected) do
-    if MapSet.size(connected) == 0 do
-      false
-    else
-      with {:ok, rank_stones} <- Map.fetch(stones, rank) do
-        winner?(stones, rank-1, find_connected_stones(connected, rank_stones))
-      else
-        :error -> false
-      end
-    end
-  end
-
-  def find_connected_stones(connected, rank_stones) do
-    find_group =  fn 
-                    elem, [] -> 
-                      {:cont, [elem]}
-                    elem, [top | _rest] = group ->
-                      if elem == top + 1 do
-                        {:cont, [elem | group]}
-                      else
-                        {:cont, group, [elem]}
-                      end
-                  end
-    after_func =  fn
-                    [] -> {:cont, []}
-                    group -> {:cont, group, []}
-                  end
-
-    rank_stones
-    |> Enum.sort
-    |> Enum.chunk_while([], find_group, after_func)
-    |> Enum.filter(&Enum.any?(&1, fn s -> s in connected or s+1 in connected end))
-    |> Enum.concat
-    |> Enum.into(MapSet.new)
+  def connected_cluster?(cluster, span, axis) do
+    {first, last} = Enum.min_max_by(cluster, &elem(&1, axis))
+    elem(last, axis) - elem(first, axis) == span
   end
 end
